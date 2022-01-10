ee6e2e3320b7312372443dc5299dd3792d4674ac
Markos <markosmk@me.com>
2022-01-10 16:20:20 -0300

    add: pages as example

---
+++ frontend/src/pages/Products/Products.js
@@ -0,0 +1,50 @@
+import { Table } from 'react-bootstrap';
+
+const Products = () => {
+  return (
+    <div>
+      <h1>Products</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, quos excepturi. Expedita
+        dolor non, saepe tempora soluta eveniet distinctio ut doloribus perferendis asperiores porro
+        odio cumque blanditiis ad, aut deleniti.
+      </p>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Producto</th>
+            <th>Price</th>
+            <th>Stock</th>
+            <th>Sales</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Gorgeous Rubber Gloves</td>
+            <td>$ 165</td>
+            <td>+32</td>
+            <td>65</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Gorgeous Plastic Tuna</td>
+            <td>$ 432</td>
+            <td>-3</td>
+            <td>145</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Tasty Concrete Hat</td>
+            <td>$ 334</td>
+            <td>98</td>
+            <td>33</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default Products;
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,2 +1,5 @@
-export { default as Home } from './Home/Home';
+export { default as Dashboard } from './Dashboard/Dashboard';
+export { default as Transactions } from './Transactions/Transactions';
+export { default as Products } from './Products/Products';
+export { default as Customers } from './Customers/Customers';
 export { default as NotFound } from './NotFound';
