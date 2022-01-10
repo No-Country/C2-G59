188cff8f13540567b95fc662b7c1bde19d792ebf
Markos <markosmk@me.com>
2022-01-10 15:22:30 -0300

    add: layout component

---
+++ frontend/src/components/layout/Layout.js
@@ -0,0 +1,26 @@
+import { Outlet } from 'react-router-dom';
+
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Footer from './Footer/Footer';
+
+const Layout = () => {
+  return (
+    <div className="d-flex">
+      {/* <!-- Sidebar --> */}
+      <Sidebar className="sidebar" />
+      <div className="p-0 d-flex flex-column wrapper">
+        {/* <!-- Header --> */}
+        <Header />
+        <div className="px-md-4 content">
+          {/* <!-- Content Page --> */}
+          <Outlet />
+        </div>
+        {/* <!-- Footer --> */}
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
