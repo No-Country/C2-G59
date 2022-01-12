fd35aeef7104bb3660b700007c8d03a9daa606e2
RocioSulca <rociosulca.z@gmail.com>
2022-01-11 23:34:00 -0500

    Add mockdata

--- frontend/src/pages/Dashboard/PartOne.js
+++ frontend/src/pages/Dashboard/PartOne.js
@@ -3,11 +3,11 @@ import { Bar, Line, Doughnut, Pie } from 'react-chartjs-2';
 
 const PartOne = () => {
   const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
     datasets: [
       {
         label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        data: [12, 19, 3, 5, 12, 3, 15],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
@@ -29,13 +29,41 @@ const PartOne = () => {
     ],
   };
 
+  const dataPo = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: ['15', '20', '15', '20', '25', '5'],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+
   return (
     <div>
       <div className="row m-3">
         <div className="col">
           <div className="card">
             <div className="card-body">
-              <h4 className="card-title">Line Chart</h4>
+              <h4 className="card-title">Ventas mensuales</h4>
               <Line data={data}
                 height={150} width={150}
               />
@@ -45,8 +73,8 @@ const PartOne = () => {
         <div className="col">
           <div className="card">
             <div className="card-body">
-              <h4 className="card-title">Pie Chart</h4>
-              <Pie data={data}
+              <h4 className="card-title">Ventas semanales</h4>
+              <Pie data={dataPo}
                 height={150} width={150}
               />
             </div>
@@ -55,7 +83,7 @@ const PartOne = () => {
         <div className="col">
           <div className="card">
             <div className="card-body">
-              <h4 className="card-title">Line Chart</h4>
+              <h4 className="card-title">Los mas vendidos</h4>
               <Bar data={data}
                 height={150} width={150}
               />
@@ -65,8 +93,8 @@ const PartOne = () => {
         <div className="col">
           <div className="card">
             <div className="card-body">
-              <h4 className="card-title">Doughnut Chart</h4>
-              <Doughnut data={data}
+              <h4 className="card-title">Porcentajes</h4>
+              <Doughnut data={dataPo}
                 height={150} width={150}
               />
             </div>
