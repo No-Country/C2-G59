88b53472aa751fad6dfce2d3c90dc8289cd6b541
Freddy Michelena <freddy@michelena.com>
2022-01-23 21:33:28 -0400

    Update controllers for PurchaseOrder and RetailSale. Create script to fill retail_sales table

--- database/app.js
+++ database/app.js
@@ -1,10 +1,7 @@
 const axios = require("axios");
 
-// Models
-const { Product } = require("../backend/db/models/product.model");
-const { getAmountTotal } = require("./helpers/calculate");
-
 // Helpers
+const { getAmountTotal } = require("./helpers/calculate");
 const {
 	getRandomInt,
 	randomProductId,
@@ -13,7 +10,7 @@ const {
 	randomBranchId,
 	randomBranchSupplierId,
 	randomSupplierInvoice,
-	randomPurchaseOrderDate,
+	randomTransactionDate,
 } = require("./helpers/generate-values");
 
 const urlMockApi =
@@ -38,12 +35,11 @@ const fillProductsTable = async () => {
 	}
 };
 
-const fillPurchasOrderTable = async () => {
-	const purchaseOrderToAdd = 90;
+const fillPurchasOrderTable = async (purchaseOrderToAdd) => {
 	for (let i = 0; i < purchaseOrderToAdd; i++) {
 		console.log(`Generating ${i+1} of ${purchaseOrderToAdd}`);
 		let products = [];
-		const { purchase_date, pay_date } = randomPurchaseOrderDate();
+		const { trx_date:purchase_date, pay_date } = randomTransactionDate();
 
 		// Generate random list of products
 		console.log('Generating random list of products...');
@@ -93,5 +89,58 @@ const fillPurchasOrderTable = async () => {
 	}
 };
 
+const fillRetailSaleTable = async (salesToAdd) => {
+	for (let i = 0; i < salesToAdd; i++) {
+		console.log(`Generating ${i+1} of ${salesToAdd}`);
+		let products = [];
+		const { trx_date:sale_date, pay_date } = randomTransactionDate();
+
+		// Generate random list of products
+		console.log('Generating random list of products...');
+		for (let i = 0; i < getRandomInt(6); i++) {
+			const { data } = await axios
+				.get(`${api}/products/${randomProductId()}`)
+				.catch((error) => console.warn(error));
+
+			const { id, price } = data.product;
+			const isProductInList = products.some(
+				(product) => product.id === id,
+			);
+
+			if (id !== null && price !== null && !isProductInList) {
+				products.push({
+					product_id: id,
+					count: getRandomInt(3),
+					price,
+				});
+			}
+		}
+
+		const retail_sale = {
+			sale_date,
+			pay_date,
+			payment_status: "complete",
+			branch_id: randomBranchId(),
+			amount: getAmountTotal(products),
+			products,
+		};
+
+		console.log("Saving sale...");
+		const resp = await axios.post(`${api}/sales`, retail_sale);
+		console.log(resp.data.retailSale);
+
+		// Lo siguiente se hizo antes de actualizar el controlador de ProductOrder
+		// para que actualizara el stock automaticamente
+		// console.log("Saving updating products stock...");
+		// for (let product of products) {
+		// 	const res = await axios.put(
+		// 		`${api}/products/${product.product_id}/stock/${product.count}?action=increment`,
+		// 	).catch( (error) => console.log(error));
+		// 	console.log(res.data.msg);
+		// }
+	}
+};
+
 // fillProductsTable();
-fillPurchasOrderTable();
+// fillPurchasOrderTable(1);
+// fillRetailSaleTable(92);
