579c48c0df50af58b210d2947e296a57e43b6a1a
Markos <markosmk@me.com>
2022-01-25 15:21:52 -0300

    update: get product items

--- frontend/src/pages/Products/Products.js
+++ frontend/src/pages/Products/Products.js
@@ -1,14 +1,24 @@
 import { Col, Container, Row } from 'reactstrap';
 import ReactHTMLTableToExcel from 'react-html-table-to-excel';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Breadcrumbs from '../../components/common/Breadcrumbs';
 import { BasicTable } from './BasicTable';
+import { useEffect } from 'react';
+import { getProducts } from '../../store/actions/productsActions';
 
 const Products = () => {
+  const { items, loading, error } = useSelector(state => state.products);
+  const dispatch = useDispatch();
+
   const breadcrumbItems = [
     { title: 'Fintech', link: '/' },
     { title: 'Products', link: '#d' },
   ];
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
 
   return (
     <div className="page-content">
@@ -29,7 +39,22 @@ const Products = () => {
                 buttonText="Export to Excel"
               />
             </div>
-            <BasicTable />
+
+            {error && (
+              <>
+                <h3>Errors: {JSON.stringify(error, null, ' ')}</h3>
+              </>
+            )}
+
+            {loading ? (
+              <>
+                <h3>Cargando...</h3>
+              </>
+            ) : (
+              <>
+                <BasicTable data={items} />
+              </>
+            )}
           </Col>
         </Row>
       </Container>
