db50716ba31483ceace3899cce01e6715ed79736
Markos <markosmk@me.com>
2022-01-25 12:23:41 -0300

    update: add container and row class

--- frontend/src/pages/Products/Products.js
+++ frontend/src/pages/Products/Products.js
@@ -1,56 +1,38 @@
-// import { Table } from 'react-bootstrap';
+import { Col, Container, Row } from 'reactstrap';
 import ReactHTMLTableToExcel from 'react-html-table-to-excel';
-// import { dataProjects } from '../../services/data';
+
+import Breadcrumbs from '../../components/common/Breadcrumbs';
 import { BasicTable } from './BasicTable';
 
 const Products = () => {
-  // const data = dataProjects.map(item => {
-  //   return (
-  //     <tbody key={item.id}>
-  //       <tr className="table-light">
-  //         <td>{item.id}</td>
-  //         <td>{item.product}</td>
-  //         <td>{item.description}</td>
-  //         <td>${item.price} </td>
-  //         <td>{item.stock}</td>
-  //         <td>{item.sales}</td>
-  //       </tr>
-  //     </tbody>
-  //   );
-  // });
+  const breadcrumbItems = [
+    { title: 'Fintech', link: '/' },
+    { title: 'Products', link: '#d' },
+  ];
 
   return (
-    <div>
-      <h1>Products</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, quos excepturi. Expedita
-        dolor non, saepe tempora soluta eveniet distinctio ut doloribus perferendis asperiores porro
-        odio cumque blanditiis ad, aut deleniti.
-      </p>
-      <div className="d-flex justify-content-end">
-        <ReactHTMLTableToExcel
-          className="btn btn-primary mb-3 "
-          table="data table"
-          filename="Product - Table"
-          sheet="tablexls"
-          buttonText="Export to Excel"
-        />
-      </div>
-      {/* <Table striped bordered hover id="data table">
-        <thead>
-          <tr className="table-primary">
-            <th>#</th>
-            <th>Product</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Stock</th>
-            <th>Sales</th>
-          </tr>
-        </thead>
-        {data}
-      </Table> */}
-
-      <BasicTable/>
+    <div className="page-content">
+      <Container fluid>
+        <Breadcrumbs title="Products" breadcrumbItems={breadcrumbItems} />
+        <Row>
+          <Col>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, quos excepturi.
+              Expedita dolor non, saepe tempora soluta
+            </p>
+            <div className="d-flex justify-content-end">
+              <ReactHTMLTableToExcel
+                className="btn btn-primary mb-3 "
+                table="data table"
+                filename="Product - Table"
+                sheet="tablexls"
+                buttonText="Export to Excel"
+              />
+            </div>
+            <BasicTable />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
