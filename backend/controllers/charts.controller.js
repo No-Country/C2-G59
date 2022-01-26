e8f07b249932c0f1a89316371bd9cf858c05859f
Freddy Michelena <freddy@michelena.com>
2022-01-25 23:07:56 -0400

    Code branches-transaction controller for charts

--- backend/controllers/charts.controller.js
+++ backend/controllers/charts.controller.js
@@ -15,7 +15,7 @@ const { toDecimal } = require("../helpers/calculate");
 
 const dateFormat = 'YYYY-MM-DD';
 
-// Get all branches total profit 
+// Get branches total profit, income and outcome
 const getBranchesProfit = async(req = request, res = response) => {
     // Query data
     let {
@@ -187,9 +187,204 @@ const getBranchesProfit = async(req = request, res = response) => {
     }
 };
 
+// Get branches total sales
+const getBranchesTransactionCount = async(req = request, res = response) => {
+    // Query data
+    let {
+        months = 3, // months > 1
+        branch_id = 0, // 0 para todas las Branch
+        purchase = true, // show purchase count
+        sales = true, // show sales count
+    } = req.query;
+
+    months = Number(months);
+    branch_id = Number(branch_id);
+    purchase = JSON.parse(purchase);
+    sales = JSON.parse(sales);
+    let labels = [];
+    let data = [];
+
+    if (months < 2) {
+        return res.status(400).json({
+            msg: 'Query months must be 2 or more'
+        });
+    }
+
+    try {
+        // Establecer mes de inicio y fin
+        let startDate = moment().subtract(months, 'months').startOf('month');
+        let endDate = moment().subtract(1, 'months').endOf('month'); // Previous month
+        let allPurchaseOrder;
+        let allRetailSales;
+
+        // Obtener todas las PurchaseOrder
+        if (purchase) {
+            allPurchaseOrder = await sequelize.query(
+                getPurchaseOrderBetweenDates(
+                    startDate.format(dateFormat),
+                    endDate.format(dateFormat),
+                    branch_id
+                ),
+                { type: QueryTypes.SELECT }
+            );
+        }
+
+        // Obtener todas las RetailSales
+        if (sales) {
+            allRetailSales = await sequelize.query(
+                getRetailSalesBetweenDates(
+                    startDate.format(dateFormat),
+                    endDate.format(dateFormat),
+                    branch_id
+                ),
+                { type: QueryTypes.SELECT }
+            );
+        }
+
+        // Asignar valores a labels
+        for(let i = 0; i < months; i++) {
+            const monthToEvaluate = startDate.clone().add(i, 'months');
+            labels.push(monthToEvaluate.format('MMMM'));
+        }
+
+        // Asignar valores a data (profit de branch(es))
+        if ( branch_id === 0 ) { // Todas las branch
+            // Obtener la informacion de todas las Branch
+            const branches = await Branch.findAll();
+            
+            for ( let branch of branches ) { // Se evaluan tolas las branch
+                let branchData = [];
+                const branchId = branch.dataValues.id;
+
+                for(let i = 0; i < months; i++) { // Se evalua todos los meses para una branch
+                    const monthToEvaluate = startDate.clone().add(i, 'months');
+                    let countPurchaseOrder = 0; // in month
+                    let countRetailSale = 0; // in month
+                    let objTemp = {};
+
+                    // Se obtienen las PurchaseOrder para la branch del loop
+                    if (purchase) {
+                        allPurchaseOrder.forEach( purchaseOrder => {
+                            let purchaseDate = moment(purchaseOrder.purchase_date);
+                            if (purchaseDate.month() === monthToEvaluate.month()
+                                && branchId === purchaseOrder.branch_id
+                            ){
+                                countPurchaseOrder++;
+                            }
+                        });
+                    }
+
+                    // Obtener el monto total de las ventas
+                    // del mes a evaluar (monthToEvaluate)
+                    if (sales) {
+                        allRetailSales.forEach( retailSale => {
+                            let saleDate = moment(retailSale.sale_date);
+                            if (saleDate.month() === monthToEvaluate.month()
+                                && branchId === retailSale.branch_id
+                            ) {
+                                countRetailSale++;
+                            }
+                        });
+                    }
+
+                    if (purchase) {
+                        objTemp = {... objTemp, purchase_count: countPurchaseOrder };
+                    }
+
+                    if (sales) {
+                        objTemp = {...objTemp, sales_count: countRetailSale };
+                    }
+
+                    branchData.push({
+                        month: monthToEvaluate.format('MMMM'),
+                        ...objTemp,
+                    });
+                
+                }
+
+                data.push({
+                    branch_id: branchId,
+                    branch_name: branch.dataValues.branch_name,
+                    data: branchData
+                });
+            }
+
+        } else {
+            // Obtener la informacion de todas las Branch
+            const branch = await Branch.findOne({ where: { id: branch_id }});
+
+            let branchData = [];
+            const branchId = branch.dataValues.id;
+
+            for(let i = 0; i < months; i++) { // Se evalua todos los meses para una branch
+                const monthToEvaluate = startDate.clone().add(i, 'months');
+                let countPurchaseOrder = 0; // in month
+                let countRetailSale = 0; // in month
+                let objTemp = {};
+
+                // Se obtienen las PurchaseOrder para la branch del loop
+                if (purchase) {
+                    allPurchaseOrder.forEach( purchaseOrder => {
+                        let purchaseDate = moment(purchaseOrder.purchase_date);
+                        if (purchaseDate.month() === monthToEvaluate.month()
+                            && branchId === purchaseOrder.branch_id
+                        ){
+                            countPurchaseOrder++;
+                        }
+                    });
+                }
+
+                // Obtener el monto total de las ventas
+                // del mes a evaluar (monthToEvaluate)
+                if (sales) {
+                    allRetailSales.forEach( retailSale => {
+                        let saleDate = moment(retailSale.sale_date);
+                        if (saleDate.month() === monthToEvaluate.month()
+                            && branchId === retailSale.branch_id
+                        ) {
+                            countRetailSale++;
+                        }
+                    });
+                }
+
+                if (purchase) {
+                    objTemp = {... objTemp, purchase_count: countPurchaseOrder };
+                }
+
+                if (sales) {
+                    objTemp = {...objTemp, sales_count: countRetailSale };
+                }
+
+                branchData.push({
+                    month: monthToEvaluate.format('MMMM'),
+                    ...objTemp,
+                });
+            }
+
+            data.push({
+                branch_id,
+                branch_name: branch.dataValues.branch_name,
+                data: branchData
+            });
+        
+        }
+
+        return res.status(200).json({
+            labels,
+            data
+        });
+
+    } catch(error) {
+        return res.status(500).json({
+            msg: 'Talk to the admin',
+            error
+        });
+    }
+};
 
 module.exports = {
-    getBranchesProfit
+    getBranchesProfit,
+    getBranchesTransactionCount
 };
 
 /**
