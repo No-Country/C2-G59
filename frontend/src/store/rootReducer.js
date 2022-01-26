4468400c5b050019d3d8e9860c04b36f62acc726
Markos <markosmk@me.com>
2022-01-26 09:19:52 -0300

    update: moved file out reducer folder

---
+++ frontend/src/store/rootReducer.js
@@ -0,0 +1,18 @@
+import { combineReducers } from 'redux';
+
+import { layoutReducer } from './reducers/layoutReducer';
+import { authReducer } from './reducers/authReducer';
+import { productsReducer } from './reducers/productsReducer';
+import { userReducer } from './reducers/userReducer';
+import { purchaseReducer } from './reducers/purchaseReducer';
+
+export const rootReducer = combineReducers({
+  // public
+  layout: layoutReducer,
+  // private
+  auth: authReducer,
+  products: productsReducer,
+  user: userReducer,
+  purchase: purchaseReducer,
+  // TODO: branchesReducer
+});
