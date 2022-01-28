e7b0a4c2c926b4921492f126e9f1e23cce084531
RocioSulca <rociosulca.z@gmail.com>
2022-01-28 12:02:16 -0500

    Add bill

--- frontend/src/pages/Purchases/PurchaseDetails.js
+++ frontend/src/pages/Purchases/PurchaseDetails.js
@@ -1,11 +1,8 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Card, CardBody, Col, Container, Row, Table, Button } from 'reactstrap';
 import { useParams, useNavigate } from 'react-router-dom';
 import { axiosWithOutToken } from '../../services/axios';
 import Breadcrumbs from '../../components/common/Breadcrumbs';
-import { getProductById } from '../../utils/dataProducts';
-import { getPurchaseById } from '../../utils/dataPurchase';
 
 const PurchaseDetails = () => {
   const breadcrumbItems = [
@@ -16,76 +13,75 @@ const PurchaseDetails = () => {
   const navigate = useNavigate();
   const params = useParams();
   const [purchase, setPurchase] = useState({});
-  console.log(params)
+  console.log(params);
   // para obtener los datos del form
   useEffect(() => {
-    axiosWithOutToken('/purchase/' + params.id).then(({ data }) => {
-      setPurchase(data);
-      console.log(data);
-    }).catch((err) => {
-      console.log(err)
-    });
+    axiosWithOutToken('/purchase/' + params.id)
+      .then(({ data }) => {
+        setPurchase(data);
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-
   let acumulador = 0;
-  purchase.products && purchase.products.forEach(item => {
-    const multipl = item.cost * item.count;
-    acumulador += multipl;
-  });
+  purchase.products &&
+    purchase.products.forEach(item => {
+      const multipl = item.cost * item.count;
+      acumulador += multipl;
+    });
 
   return (
     <div className="page-content">
-    <Container fluid>
-      <Breadcrumbs title="Purchase Details" breadcrumbItems={breadcrumbItems} />
-      <Row>
-        <Col>
-          <Card>
-            <CardBody>
-            <div className="m-5 wrapperBranches">
-              <div className="d-flex justify-content-between align-items-start">
-                <h1>Purchase Details: {params.id}</h1>
-                <Button variant="warning" onClick={() => navigate(-1)}>
-                  Back
-                </Button>
-              </div>
-              <hr />
-              {/* <h2>CLIENT: {data.name}</h2> */}
-              <h2>List of products:</h2>
+      <Container fluid>
+        <Breadcrumbs title="Purchase Details" breadcrumbItems={breadcrumbItems} />
+        <Row>
+          <Col>
+            <Card>
+              <CardBody>
+                <div className="m-5 wrapperBranches">
+                  <div className="d-flex justify-content-between align-items-start">
+                    <h1>Purchase Details: {params.id}</h1>
+                    <Button variant="warning" onClick={() => navigate(-1)}>
+                      Back
+                    </Button>
+                  </div>
+                  <hr />
+                  <h2>List of products:</h2>
 
-              <Table striped bordered hover size="sm">
-                <thead>
-                  <tr className="thead-dark">
-                    <th>Product</th>
-                    <th>Price</th>
-                    <th>Stock</th>
-                    <th>Subtotal</th>
-                  </tr>
-                </thead>
-                {purchase.products && purchase.products.map((item, index) => {
-                  return (
-                    <tbody className="table light" key={item.ProductId}>
-                      <tr key={index}>
-                        <td>{item.product_name}</td>
-                        <td>${item.cost} </td>
-                        <td>{item.count}</td>
-                        <td>$ {Math.round(item.cost * item.count)}</td>
+                  <Table striped bordered hover size="sm">
+                    <thead>
+                      <tr className="thead-dark">
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Stock</th>
+                        <th>Subtotal</th>
                       </tr>
-                    </tbody>
-                  );
-                })}
-              </Table>
-
-              <h2>Total $ {' '}
-                {Math.round(acumulador)}
-
-              </h2>
-            </div>
-          </CardBody>
-        </Card>
-      </Col>
-    </Row>
-    </Container>
+                    </thead>
+                    {purchase.products &&
+                      purchase.products.map((item, index) => {
+                        return (
+                          <tbody className="table light" key={item.ProductId}>
+                            <tr key={index}>
+                              <td>{item.product_name}</td>
+                              <td>${item.cost} </td>
+                              <td>{item.count}</td>
+                              <td>$ {Math.round(item.cost * item.count)}</td>
+                            </tr>
+                          </tbody>
+                        );
+                      })}
+                  </Table>
+                  <h2>Total $ {Math.round(acumulador)}</h2>
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
--- frontend/src/pages/Purchases/Purchases.js
+++ frontend/src/pages/Purchases/Purchases.js
@@ -7,29 +7,6 @@ import Breadcrumbs from '../../components/common/Breadcrumbs';
 import { useSelector, useDispatch } from 'react-redux';
 import { getPurchases } from '../../store/actions/purchaseActions';
 
-// data example
-// import { getAllPurchases } from '../../utils/dataPurchase';
-
-// const dataPurchases = getAllPurchases();
-
-// const initialList = [
-//   // {
-//   //   id: '1',
-//   //   name: 'Product 2',
-//   //   ProductId: 3,
-//   //   amount: 44,
-//   //   price: 133,
-//   //   subTotal: 43553,
-//   // },
-//   // {
-//   //   id: '2',
-//   //   name: 'Product 4',
-//   //   ProductId: 5,
-//   //   amount: 3,
-//   //   price: 33444,
-//   //   subTotal: 103422,
-//   // },
-// ];
 
 const Purchases = () => {
   dayjs.extend(localizedFormat);
@@ -44,36 +21,8 @@ const Purchases = () => {
 
   useEffect(() => {
     dispatch(getPurchases());
-  }, []);
-
-  // console.log(purchaseData);
-
-  // const [listProducts, setListProducts] = useState(initialList);
-  // const [total, setTotal] = useState(0.0);
-
-  // useEffect(() => {
-  //   let sum = 0;
-
-  //   for (let i = 0; i < listProducts.length; i++) {
-  //     sum += listProducts[i].subTotal;
-  //   }
-  //   setTotal(sum);
-  //   console.log(listProducts);
-  // }, [listProducts]);
-
-  // const handleAddProduct = () => {
-  //   setListProducts([
-  //     ...listProducts,
-  //     {
-  //       id: Math.random(),
-  //       name: '',
-  //       ProductId: 0,
-  //       amount: 1,
-  //       price: 0,
-  //       subTotal: 0,
-  //     },
-  //   ]);
-  // };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);  
 
   return (
     <div className="page-content">
@@ -109,11 +58,6 @@ const Purchases = () => {
                       <tr key={item.id}>
                         <td>{index}</td>
                         <td>supplier</td>
-                        {/* <td>
-                {new Intl.NumberFormat('es-AR', { style: 'currency', currency: 'ARS' }).format(
-                  item.price,
-                )}
-              </td> */}
                         <td>{item.invoice}</td>
                         <td>$ {item.amount}</td>
                         <td>{dayjs(item.pay_date).format('LLL')}</td>
@@ -133,7 +77,6 @@ const Purchases = () => {
             </Card>
           </Col>
         </Row>
-
       </Container>
     </div>
   );
